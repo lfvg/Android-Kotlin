155eff5e18a98c08ea5af97845ffd21f19ab5629
Felipe Véras <lfvg@cin.ufpe.br>
2018-08-18 19:09:29 -0300

    Parte 1
    Incrementos na main activity

--- app/src/main/java/com/redflag/lfvg/kotlin/MainActivity.kt
+++ app/src/main/java/com/redflag/lfvg/kotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package com.redflag.lfvg.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var ctr = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nao_clique_button.setOnClickListener{
+            if(ctr){
+                textView.setText(R.string.told_you_so)
+            }
+            else{
+                textView.setText(R.string.hello_h)
+            }
+            ctr = !ctr
+        }
     }
 }
